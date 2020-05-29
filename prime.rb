8528f190c497ce8791861317390ef955feb831c5
Samantha Wabi <swfullpotential@gmail.com>
2020-05-29 00:35:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,9 @@
-# def prime?(integer)
-#     i = 2
-#     while i < integer
-#       return false if integer <= 1
-#         i += 1
-#     end
-#     true
-# end
-
 def prime?(integer)
-  n = 2
-  while n < integer
-    return false if integer % n <= 0 
-    n += 1
-  end
-  true
+    i = 2
+    while i < integer
+      return false if integer <= 0 
+        i += 1
+    end
+    true
 end
+
